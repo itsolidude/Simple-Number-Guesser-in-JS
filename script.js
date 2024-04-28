e7426e3b477b9b0a204668eeea1170c9aaa7731e
itsolidude <flexibleoli.12@gmail.com>
2024-04-27 20:17:40 -0400

    added game logic

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
-const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+let SecretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = SecretNumber;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -10,9 +9,14 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number! 🎉';
   } else if (guess === SecretNumber) {
+    document.querySelector('.number').textContent = SecretNumber;
     document.querySelector('.message').textContent = 'Correct Number';
-    score = score + 2;
-    document.querySelector('.score').textContent = score;
+    document.querySelector('body ').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > SecretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
@@ -32,5 +36,16 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = 'You lost the game 💥';
       document.querySelector('.score').textContent = '0';
+      document.querySelector('.guess').value = '';
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  SecretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body ').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
